fa5a4e589166eeec457a028f0d84e0584e28445e
Natalia Mryka <natalia8517@gmail.com>
2020-11-10 10:31:13 +0100

    poprawki w zadaniach

--- kodilla-intro/src/main/java/LeapYear.java
+++ kodilla-intro/src/main/java/LeapYear.java
@@ -1,10 +1,10 @@
 public class LeapYear {
     public static void main(String[] args){
-        int year = 2020;
+        int year = 2024;
         checkIfItsLeapYear(year);
     }
     public static void checkIfItsLeapYear(int yearNumber){
-        if (yearNumber%4==0 && yearNumber%100==0 && yearNumber%400==0 || yearNumber%4==0 && yearNumber%100!=0){
+        if ((yearNumber%4==0 && yearNumber%100==0 && yearNumber%400==0) || (yearNumber%4==0 && yearNumber%100!=0)){
             System.out.println(yearNumber + " is a leap year");
         }
         else{
--- kodilla-intro/src/main/java/MainExtended.java
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -3,10 +3,15 @@ public class MainExtended {
         int parrotAge = 1;
         String parrotName = "Charlie";
         boolean isAnimal = true;
+        boolean isBird = true;
         char firstLetter = 'C';
+        double height = 0.15;
         System.out.println(parrotAge);
         System.out.println(parrotName);
         System.out.println(isAnimal);
         System.out.println(firstLetter);
+        System.out.println(isBird);
+        System.out.println(height);
+
     }
 }
