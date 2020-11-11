537bb850955042a7415304b4d129fb5739e53cb8
Natalia Mryka <natalia8517@gmail.com>
2020-11-11 14:15:12 +0100

    Zadanie: zamiana kodu struktoralnego na obiektowy

--- kodilla-intro/src/main/java/Application.java
+++ kodilla-intro/src/main/java/Application.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class Application {
+public class Application {
+
+    String name;
+    double age;
+    double height;
+
+    public Application(String name, double age, double height){
+        this.name = name;
+        this.age = age;
+        this.height = height;
+    }
+    public static void main(String[] args) {
+
+        Application newApplication = new Application("Adam", 40.5, 178);
+
+
+        if (newApplication.name != null) {
+            if (newApplication.age > 30 && newApplication.height > 160) {
+                System.out.println("User is older than 30 and higher than 160cm");
+            } else {
+                System.out.println("User is 30 (or younger) or 160cm (or shorter)");
+            }
+        }
+    }
 }
