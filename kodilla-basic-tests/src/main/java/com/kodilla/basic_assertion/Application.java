81989ad96e1699791b5cbb7cd0983577061bca9f
Natalia Mryka <natalia8517@gmail.com>
2020-11-19 11:11:26 +0100

    zadanie calculator

---
+++ kodilla-basic-tests/src/main/java/com/kodilla/basic_assertion/Application.java
@@ -0,0 +1,32 @@
+package com.kodilla.basic_assertion;
+
+public class Application {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        int a = 5;
+        int b = 8;
+        int sumResult = calculator.sum(a,b);
+        int substractResult = calculator.subtract(a,b);
+        boolean correct = ResultChecker.assertEquals(13, sumResult);
+        boolean isSubtractResultCorrect = ResultChecker.assertEquals(-3, substractResult);
+        boolean isSquareResultCorrect = ResultChecker.assertEquals(25, calculator.raiseToSquare(a));
+
+        if (correct) {
+            System.out.println("Metoda sum działa poprawnie dla liczb " + a + " i " + b);
+        } else {
+            System.out.println("Metoda sum nie działa poprawnie dla liczb " + a + " i " + b);
+        }
+
+        if (isSubtractResultCorrect) {
+            System.out.println("Metoda subtract działa poprawnie dla liczb " + a + " i " + b);
+        } else {
+            System.out.println("Metoda subtract nie działa poprawnie dla liczb " + a + " i " + b);
+        }
+
+        if (isSquareResultCorrect) {
+            System.out.println("Metoda raise to square działa poprawnie dla liczby " + a);
+        } else {
+            System.out.println("Metoda raise to square nie działa poprawnie dla liczby " + a );
+        }
+    }
+}
