16683c60921b6bcd9fc99203ed227568ccbc1396
Natalia Mryka <natalia8517@gmail.com>
2020-12-08 10:37:41 +0100

    fix

--- kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/maps/homework/SchoolDirectory.java
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/maps/homework/SchoolDirectory.java
@@ -7,17 +7,9 @@ public class SchoolDirectory {
     public static void main(String[] args) {
         Map<Principal, School> schoolBoard = new HashMap<>();
 
-        School school1 = new School("School 1", 30, 28, 21, 29);
-        School school2 = new School("School 2", 31, 28, 30, 25);
-        School school3 = new School("School 3", 34, 28, 32, 30);
-
-        Principal johnS = new Principal("John Smith");
-        Principal emilyJ = new Principal("Emily Jones");
-        Principal willW = new Principal("Will Watson");
-
-        schoolBoard.put(johnS, school1);
-        schoolBoard.put(emilyJ, school2);
-        schoolBoard.put(willW, school3);
+        schoolBoard.put(new Principal("John Smith"), new School("School 1", 30, 28, 21, 29));
+        schoolBoard.put(new Principal("Emily Jones"), new School("School 2", 31, 28, 30, 25));
+        schoolBoard.put(new Principal("Will Watson"), new School("School 3", 34, 28, 32, 30));
 
         for (Map.Entry<Principal, School> schoolBoardEntry : schoolBoard.entrySet()) {
             System.out.println("Principal " + schoolBoardEntry.getKey().getPrincipalName() + "\nSchool name " + schoolBoardEntry.getValue().getName() + " \nTotal num of students " + schoolBoardEntry.getValue().totalNumberOfStudents());
